66f040e38f639ba7a42a60f732599fd1ea02e1c6
Calebe Machado <calebe.machado@ifood.com.br>
2024-07-01 21:15:47 -0300

    Initialize project with base dependencies

---
+++ src/main/java/com/hostfully/booking/api/BookingApi.java
@@ -0,0 +1,13 @@
+package com.hostfully.booking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApi.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostfully/booking/api/BookingApiTests.java
@@ -0,0 +1,13 @@
+package com.hostfully.booking.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
