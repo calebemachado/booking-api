101a14df9eb5eba5e85eafb5dda3d09554035172
Calebe Machado <calebe.machado@ifood.com.br>
2024-07-01 23:59:39 -0300

    Refactor Person adding functionality and unit testing

--- src/main/java/com/hostfully/booking/api/core/domain/Person.java
+++ src/main/java/com/hostfully/booking/api/core/domain/Person.java
@@ -1,10 +1,69 @@
 package com.hostfully.booking.api.core.domain;
 
-import java.util.UUID;
+import lombok.Getter;
 
-public record Person(
-        UUID id,
-        String name,
-        String surname
-) {
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+@Getter
+public class Person extends BaseEntity {
+
+    private final String name;
+    private final String surname;
+    private final Boolean active;
+
+    private Person(
+            final UUID id,
+            final String name,
+            final String surname,
+            final Boolean active
+    ) {
+        super(id);
+        this.name = name;
+        this.surname = surname;
+        this.active = active;
+    }
+
+    private Person(
+            final UUID id,
+            final String name,
+            final String surname,
+            final Boolean active,
+            final LocalDateTime createdAt,
+            final LocalDateTime updatedAt
+    ) {
+        super(id, createdAt, updatedAt);
+        this.name = name;
+        this.surname = surname;
+        this.active = active;
+    }
+
+    public static Person create(
+            final String name,
+            final String surname
+    ) {
+        Pattern pattern = Pattern.compile("^[\\p{L} '-]+$");
+
+        if (name == null || name.isBlank() || !pattern.matcher(name).matches()) {
+            throw new Error("Invalid name");
+        }
+
+        if (surname == null || surname.isBlank() || !pattern.matcher(surname).matches()) {
+            throw new Error("Invalid surname");
+        }
+
+        return new Person(UUID.randomUUID(), name, surname, true);
+    }
+
+    public static Person restore(
+            final UUID id,
+            final String name,
+            final String surname,
+            final Boolean active,
+            final LocalDateTime createdAt,
+            final LocalDateTime updatedAt
+    ) {
+        return new Person(id, name, surname, active, createdAt, updatedAt);
+    }
 }
---
+++ src/test/java/com/hostfully/booking/api/unit/PersonTest.java
@@ -0,0 +1,61 @@
+package com.hostfully.booking.api.unit;
+
+import com.hostfully.booking.api.core.domain.Person;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PersonTest {
+
+    public static final String INVALID_NAME_MSG = "Invalid name";
+    public static final String INVALID_SURNAME_MSG = "Invalid surname";
+
+    @Test
+    void shouldCreatePerson() {
+        Person person = Person.create("John", "Doe");
+        assertThat(person.getId()).isNotNull();
+        assertThat(person.getCreatedAt()).isNotNull();
+        assertThat(person.getUpdatedAt()).isNotNull();
+        assertTrue(person.getActive());
+    }
+
+    @Test
+    void shouldNotCreatePersonWithInvalidNames() {
+        Error error = assertThrows(Error.class, () -> Person.create("", "Doe"));
+        assertEquals(INVALID_NAME_MSG, error.getMessage());
+
+        error = assertThrows(Error.class, () -> Person.create("John", ""));
+        assertEquals(INVALID_SURNAME_MSG, error.getMessage());
+
+        error = assertThrows(Error.class, () -> Person.create("John", "Doe1"));
+        assertEquals(INVALID_SURNAME_MSG, error.getMessage());
+
+        error = assertThrows(Error.class, () -> Person.create("John21312", "Doe1"));
+        assertEquals(INVALID_NAME_MSG, error.getMessage());
+    }
+
+    @Test
+    public void testRestore() {
+        UUID id = UUID.randomUUID();
+        String name = "John";
+        String surname = "Doe";
+        Boolean active = true;
+        LocalDateTime createdAt = LocalDateTime.of(2023, 1, 1, 12, 0);
+        LocalDateTime updatedAt = LocalDateTime.of(2023, 1, 2, 12, 0);
+
+        Person person = Person.restore(id, name, surname, active, createdAt, updatedAt);
+
+        assertEquals(id, person.getId());
+        assertEquals(name, person.getName());
+        assertEquals(surname, person.getSurname());
+        assertEquals(active, person.getActive());
+        assertEquals(createdAt, person.getCreatedAt());
+        assertEquals(updatedAt, person.getUpdatedAt());
+    }
+}
