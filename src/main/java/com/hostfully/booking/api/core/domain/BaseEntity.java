2d193ee6172c84e3c12fcaa4d4beb90aee43a0c8
Calebe Machado <calebe.machado@ifood.com.br>
2024-07-01 23:59:14 -0300

    Create BaseEntity to represent common properties between domains

---
+++ src/main/java/com/hostfully/booking/api/core/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.hostfully.booking.api.core.domain;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class BaseEntity {
+    private final UUID id;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public BaseEntity(final UUID id) {
+        this.id = id;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public BaseEntity(
+            final UUID id,
+            final LocalDateTime createdAt,
+            final LocalDateTime updatedAt
+    ) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
